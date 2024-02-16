e72aaa8ed2d7d8a6cbf29ec726f6244aa1734e0d
Ryan Delucchi <ryan@fp.engineering>
2024-02-16 14:58:19 -0800

    started exercises

---
+++ Scans.hs
@@ -0,0 +1,69 @@
+module Scans where
+    
+    inf :: [a] -> [a]
+    -- inf la = take 500 $ la
+    inf = id
+
+    fibs = inf(1 : scanl (+) 1 fibs)
+    fibs20 = take 20 $ fibs
+    fibsLessThan100 = [x | x <- fibs20, x < 100]
+    fibsLessThan100a = filter (<100) fibs20
+
+    factorial = inf(scanl (*) 1 [1..])
+
+    stops  = "pbtdkg"
+    vowels = "aeiou"
+
+    stopVowelStop = [s1 : v : [s2] | s1 <- stops, v <- vowels, s2 <- stops]
+    stopVowelStopP = [s1 : v : [s2] | s1 <- stops, s1 == 'p', v <- vowels, s2 <- stops]
+
+    nouns = ["dog", "alien", "pickle", "donut", "chicken"]
+    verbs = ["dances", "explodes", "transforms", "tickles", "devours"]
+
+    nounVerbNoun = [n1 ++ " " ++ v ++ " " ++ n2 | n1 <- nouns, v <- verbs, n2 <- nouns]
+
+    avg x = div (sum (map length (words x)))
+        (length (words x))
+
+    avg2 x = fromIntegral (sum (map length (words x))) /
+        fromIntegral (length (words x))
+
+    myOr :: [Bool] -> Bool
+    myOr = foldr (||) False
+
+    myAny :: (a -> Bool) -> [a] -> Bool
+    myAny f = foldr ((||) . f) False
+
+    myElem :: Eq a => a -> [a] -> Bool
+    myElem a = foldr ((||) . (a==)) False
+    
+    myElem2 :: Eq a => a -> [a] -> Bool
+    myElem2 a = any (a==) 
+
+    myReverse :: [a] -> [a]
+    myReverse [] = []
+    
+    myReverse (x:xs) = myReverse xs ++ [x]
+
+    myReverse2 :: [a] -> [a]
+    myReverse2 = foldl (flip (:)) []
+
+    myReverse3 :: [a] -> [a]
+    myReverse3 = foldr (flip (++).(:[])) []
+
+    myMap :: (a -> b) -> [a] -> [b]
+    myMap f = foldr ((:) . f) []
+
+    myFilter :: (a -> Bool) -> [a] -> [a]
+    myFilter f = foldr ((++) . pick f) []
+        where pick :: (a -> Bool) -> a -> [a]
+              pick func a = [a | func a]
+
+
+    vap :: (a -> Bool) -> a -> (a, Bool)
+    vap f a = (a, f a)
+
+    myFilter2 f = foldr ((++) . (\a -> [a | f a])) []
+
+
+
