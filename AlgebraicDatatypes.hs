34d49a72b52c013011234f718ecd5b0b3d7bca62
Ryan Vimba <ryan.vimba@paradigm.inc>
2024-04-10 13:54:38 -0500

    april 10

--- AlgebraicDatatypes.hs
+++ AlgebraicDatatypes.hs
@@ -234,12 +234,89 @@ module AlgebraicDatatypes where
     
     type Converter = Quantum -> Maybe Bool
 
-    convertSingle :: Quantum -> Bool -> Maybe Bool
-    convertSingle q' b  = (\q -> if(q == q') then Just(b) else Nothing)    
+    -- convertSingle :: Quantum -> Bool -> Maybe Bool
+    -- convertSingle q' b  = (\q -> if(q == q') then Just(b) else Nothing)    
     
-    convertN :: [Converter]
-    [convertSingle(q', b) | q' <- [Yes, No, Both], b <- [True, False]]
+    -- convertN :: [Converter]
+    -- [convertSingle(q', b) | q' <- [Yes, No, Both], b <- [True, False]]
+
+    data Quad = One | Two | Three | Four deriving (Eq, Show)
+
+    eQuad :: Either Quad Quad
+    eQuad = Left One  -- Left One, Left Two, Left Three, Left Four, Right One, Right Two, Right Three, Right Four
+
+    data BinaryTree a = Leaf | Node (BinaryTree a) a (BinaryTree a) deriving (Eq, Ord, Show)
+
+    mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+    mapTree _ Leaf = Leaf
+    mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+    testTree' :: BinaryTree Integer
+    testTree' = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+    mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+    mapOkay = if mapTree (+1) testTree' == mapExpected
+              then print "yup okay!"
+              else error "test failed!"
+
+    preorder :: BinaryTree a -> [a]
+    preorder Leaf = []
+    preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+    inorder :: BinaryTree a -> [a]
+    inorder Leaf = []
+    inorder (Node left a right) = inorder left ++ [a] ++ inorder right
+
+    postorder :: BinaryTree a -> [a]
+    postorder Leaf = []
+    postorder (Node left a right) = postorder left ++ postorder right ++ [a]
+
+    testTree :: BinaryTree Integer
+    testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+    testPreorder :: IO ()
+    testPreorder = if preorder testTree == [2, 1, 3]
+                    then putStrLn "Preorder fine!"
+                    else putStrLn "Bad news bears."
+
+    testInorder :: IO ()
+    testInorder = if inorder testTree == [1, 2, 3]
+                    then putStrLn "Inorder fine!"
+                    else putStrLn "Bad news bears."
+
+    testPostorder :: IO ()
+    testPostorder = if postorder testTree == [1, 3, 2]
+                    then putStrLn "Postorder fine!"
+                    else putStrLn "postorder failed check"
+
+    main :: IO ()
+    main = do
+      testPreorder
+      testInorder
+      testPostorder
+
+    foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+    foldTree _ b Leaf = b
+    foldTree f b (Node left a right) = foldTree f (f a (foldTree f b left)) right
+
+    vigenereCipher :: String -> String -> String
+    vigenereCipher "" _ = ""
+    vigenereCipher (x : xs) keyword = undefined
+      where extendedKeyword = matchKeywordLength keyword (length xs + 1)
+    
+    -- getShiftAmount :: String -> Int -> Int
+    -- getShiftAmount keyword index = 
+
+    matchKeywordLength :: String -> Int -> String
+    matchKeywordLength keyword inputLength
+      | length keyword < inputLength = matchKeywordLength (keyword ++ keyword) inputLength
+      | otherwise = take inputLength keyword
+
+
+
 
     
+
 
 
