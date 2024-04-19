d47266ffec1c19a8909e2ef57c485575daf0052b
Ryan Vimba <ryan.vimba@paradigm.inc>
2024-04-19 09:02:17 -0500

    phone exercise

--- AlgebraicDatatypes.hs
+++ AlgebraicDatatypes.hs
@@ -367,3 +367,57 @@ module AlgebraicDatatypes where
     strip s = T.unpack $ T.strip (T.pack s)
 
 
+    type In = Char
+    type Out = Char
+    type Cycle = [Out]
+    type Presses = Int
+    type KeyVector = (In, Presses)
+    type Key = (In, Cycle)
+    
+    keyPath :: KeyVector -> Cycle -> (In, Out)
+    keyPath v@(i, p) c = (i, c !! mod p (length c)) 
+
+    newtype Telephono = Telephono [(Out, KeyVector)]
+
+    -- tKeyPad = Telephono []
+
+
+    
+    newtype DaPhone = DaPhone [(Out, KeyVector)] deriving Show
+    
+    
+
+    -- keyCycle :: [Char] -> Int -> Char
+    -- keyCycle chars presses = chars !! mod presses (length chars)  
+
+    keyPad = DaPhone [
+      ('a', ('2', 1)), ('b', ('2', 2)), ('c', ('2', 3)), ('d', ('3', 1)), 
+      ('e', ('3', 2)), ('f', ('3', 3)), ('g', ('4', 1)), ('h', ('4', 2)), 
+      ('i', ('4', 3)), ('j', ('5', 1)), ('k', ('5', 2)), ('l', ('5', 3)), 
+      ('m', ('6', 1)), ('n', ('6', 2)), ('o', ('6', 3)), ('p', ('7', 1)), 
+      ('q', ('7', 2)), ('r', ('7', 3)), ('s', ('7', 4)), ('t', ('8', 1)), 
+      ('u', ('8', 2)), ('v', ('8', 3)), ('w', ('9', 1)), ('x', ('9', 2)), 
+      ('y', ('9', 3)), ('z', ('9', 4)), ('^', ('*', 1)), ('+', ('0', 1)), 
+      (' ', ('0', 2)), ('.', ('#', 1)), (',', ('#', 2)), ('0', ('0', 3)), 
+      ('1', ('1', 1)), ('2', ('2', 4)), ('3', ('3', 4)), ('4', ('4', 4)), 
+      ('5', ('5', 4)), ('6', ('6', 4)), ('7', ('7', 7)), ('8', ('8', 4)), 
+      ('9', ('9', 5))] 
+
+    convo :: [String]
+    convo = ["Wanna play 20 questions", "Ya", "U 1st haha", "Lol OK. Have u ever tasted alcohol", "Lol ya", "Wow ur cool haha. Ur turn", "OK. Do u think I am pretty Lol", "Lol ya", "Just making sure rofl ur turn"]
+
+    type Digit = Char
+
+    reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
+    reverseTaps (DaPhone daphone) ch
+      | ch == toLower ch = [snd phoneElem]
+      | otherwise = ('*', 1) : [snd phoneElem]
+      where 
+        phoneElem :: (Out, KeyVector)
+        phoneElem = head $ filter matchLetter daphone
+        matchLetter :: (Out, KeyVector) -> Bool
+        matchLetter elem = fst elem == toLower ch
+
+    cellPhonesDead :: DaPhone -> String -> [(Digit, Presses)]
+    cellPhonesDead daphone = concatMap (reverseTaps daphone)
+
